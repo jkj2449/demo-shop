0c2e8364ebbf47e0178db414e7a1e133f3c1cb65
jkj2449 <jkj8982@gmail.com>
2020-05-30 18:28:13 +0900

    init

---
+++ src/main/java/com/board/demo/controller/SampleController.java
@@ -0,0 +1,12 @@
+package com.board.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+}
